d32ad07399046c91dfb9fac0aefe6746c17ed202
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-08-04 16:34:45 +0700

    Sesi 2 - Mengubah bentuk soal (menambahkan gua kosong dan satu goa yang ada karakter CUYPY nya)

--- Main.py
+++ Main.py
@@ -6,9 +6,16 @@ cuypy_position = random.randint(1, 4)
 print(welcome_message);
 
 name_of_user_input = input("Kenalan dong, tulis namamu dibawah!\n-> ")
+
+bentuk_goa = "|_|"
+goa_kosong = [bentuk_goa] * 4 # INI TETAP KOSONG
+goa = goa_kosong.copy() # DISINI TEMPAT CUYPY
+
+goa[cuypy_position - 1] = "|0_0|"
+
 print(f'''
 Hi {name_of_user_input}! Coba perhatikan goa dibawah ini
-|_| |_| |_| |_|
+{goa_kosong}
 ''')
 
 user_choice = int(input("Menurut kamu di gua nomor berapa CUYPY berada? [1 / 2 / 3 / 4]"))
@@ -16,9 +23,9 @@ confirmation_choice = input(f"Apakah kamu yakin memilih nomor {user_choice} [y /
 
 if confirmation_choice == "y":
 	if user_choice == cuypy_position:
-		print(f"Betul sekali {name_of_user_input}, jawabannya adalah nomor {cuypy_position}")
+		print(f"Betul sekali {name_of_user_input}, jawabannya adalah nomor {cuypy_position}, posisinya ada di \n{goa}")
 	else:
-		print(f"Coba lagi {name_of_user_input}")
+		print(f"Kamu salah, posisinya ada di {goa}\nCoba lagi {name_of_user_input}")
 elif confirmation_choice == "n":
 	print("Bye bye...")
 	quit()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CUYPY GAMES
 Source by Youtube __[Cuy University Semester 5](https://youtube.com/playlist?list=PLc6SEcJkQ6DwUhzOcOKmqysBF2FGvQZ0V&feature=shared)__
+
+### SESI 1	: Belajar Python untuk Pemula
+- Tugas : Membuat peringatan konfirmasi pilihan sebelum diproses ke hasil
+
+### SESI 2	: Belajar Python - Fundamental Logika
+- Tugas : Menampilkan list array tanpa terlihat kurung list arraynya
