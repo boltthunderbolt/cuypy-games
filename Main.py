bfee0ea5efb989d55a8945290a47dc3248f8a400
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-08-05 12:56:45 +0700

    Membuat hiasan dan teks welcome sendiri dengan membuat library sendiri di libs.py, konfirmasi user jika ingin bermain kembali atau berhenti menggunakan while loop, update README.md, membuat variable posisi start dan akhir sebuah goa agar mengubahnya jadi lebih mudah

--- Main.py
+++ Main.py
@@ -1,35 +1,46 @@
 # CUPPY GAME
 import random
+from libs import welcome_message
 
-welcome_message = "******************************************\n\n\tWELCOME TO CUPPY GAME!\t\n\n******************************************"
-cuypy_position = random.randint(1, 4)
-print(welcome_message);
-
+welcome_message("WELCOME TO CUYPY GAMES")
 name_of_user_input = input("Kenalan dong, tulis namamu dibawah!\n-> ")
 
-bentuk_goa = "|_|"
-goa_kosong = [bentuk_goa] * 4 # INI TETAP KOSONG
-goa = goa_kosong.copy() # DISINI TEMPAT CUYPY
-goa[cuypy_position - 1] = "|0_0|"
+while name_of_user_input == "":
+	name_of_user_input = input("Isi dulu namamu!\n->")
 
-goa_kosong = ' '.join(goa_kosong)
-goa = ' '.join(goa)
+while True:
+	start_position, end_position = 1, 4
+	cuypy_position = random.randint(start_position, end_position)
 
-print(f'''
-Hi {name_of_user_input}! Coba perhatikan goa dibawah ini
-{goa_kosong}
-''')
+	bentuk_goa = "|_|"
+	goa_kosong = [bentuk_goa] * 4 # INI TETAP KOSONG
+	goa = goa_kosong.copy() # DISINI TEMPAT CUYPY
+	goa[cuypy_position - 1] = "|0_0|"
 
-user_choice = int(input("Menurut kamu di gua nomor berapa CUYPY berada? [1 / 2 / 3 / 4]"))
-confirmation_choice = input(f"Apakah kamu yakin memilih nomor {user_choice} [y / n]?")
+	goa_kosong = ' '.join(goa_kosong)
+	goa = ' '.join(goa)
 
-if confirmation_choice == "y":
-	if user_choice == cuypy_position:
-		print(f"Betul sekali {name_of_user_input}, jawabannya adalah nomor {cuypy_position}, posisinya ada di \n{goa}")
+	print(f'''
+	Hi {name_of_user_input}! Coba perhatikan goa dibawah ini
+	{goa_kosong}
+	''')
+
+	user_choice = int(input("Menurut kamu di gua nomor berapa CUYPY berada? [1 / 2 / 3 / 4]\n-> "))
+	confirmation_choice = input(f"Apakah kamu yakin memilih nomor {user_choice} [y / n]?")
+
+	if confirmation_choice == "y":
+		if user_choice == cuypy_position:
+			print(f"Betul sekali {name_of_user_input}, jawabannya adalah nomor {cuypy_position}, posisinya ada di \n{goa}")
+		else:
+			print(f"Kamu salah, posisinya ada di {goa}\nCoba lagi {name_of_user_input}")
+	elif confirmation_choice == "n":
+		print("Bye bye...")
+		exit()
 	else:
-		print(f"Kamu salah, posisinya ada di {goa}\nCoba lagi {name_of_user_input}")
-elif confirmation_choice == "n":
-	print("Bye bye...")
-	quit()
-else:
-	print("Silahkan Ulangi Program")
+		print("Silahkan Ulangi Program")
+
+	play_again = input("\n\nApakah ingin melanjutkan lagi? [y / n]")
+	if play_again == "n":
+		break;
+
+print("Program selesai")
